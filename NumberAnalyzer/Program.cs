2e373a08cc77c71327d085a70adbb2f8ea191357
AliAlHashemi14 <alialhashemi14@gmail.com>
2022-06-22 17:08:49 -0400

    updated

--- NumberAnalyzer/Program.cs
+++ NumberAnalyzer/Program.cs
@@ -1,13 +1,30 @@
-﻿string runTime = "";
+﻿string runTime = "Y";
+
+//Greet and take response
+Console.WriteLine("Hello and welcome to the number analyzer. What is your name?");
+string userName = Console.ReadLine();
+
 do
 {
 
 
-    //Greet and take response
-    Console.WriteLine("Welcome to the number analyzer. Please enter a number between 1 and 100.");
-    int response = int.Parse(Console.ReadLine());
 
-    //take different actions and output
+    Console.WriteLine("Okay, " + userName + ". Please enter a number between 1 and 100.");
+    int response = 0;
+
+    //validate that the number is positive and between specified range. If not, prompt again
+    while( int.TryParse(Console.ReadLine(), out response) != true )
+    {
+        Console.WriteLine("Please enter a positive number between 1 and 100.");
+        
+    }
+    if( response > 100 || response < 0)
+    {
+        continue;
+    }
+
+
+    //take different actions and give output
 
     if (response % 2 == 1 && response < 60)
     {
@@ -32,7 +49,7 @@ do
 
 
     //prompt to end the program
-    Console.WriteLine("Would you like to analyze another number? \nPlease type 'Y' to continue and hit any other key to end the program.");
+    Console.WriteLine(userName + ", Would you like to analyze another number? \nPlease type 'Y' to continue and hit any other key to end the program.");
     runTime = Console.ReadLine();
     
 
